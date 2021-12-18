ed6bcdde45454fb1dce9dd9358ab828c35929519
Ankur Bhambri <ankurwave09@gmail.com>
2021-12-18 15:02:29 +0530

    Update settings.py

--- LaundryMgmt/laundry/settings.py
+++ LaundryMgmt/laundry/settings.py
@@ -137,5 +137,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vinodbhambri1@gmail.com'
-EMAIL_HOST_PASSWORD = 'micromax@007'
+EMAIL_HOST_USER = 'email_address'
+EMAIL_HOST_PASSWORD = 'password'
